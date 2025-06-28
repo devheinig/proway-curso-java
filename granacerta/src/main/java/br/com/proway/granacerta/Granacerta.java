e6958b010ca1ccd11fe3656e0beb99e1e5856db6
Lucas Heinig <lucasfheinig@gmail.com>
2025-06-28 12:49:57 -0300

    Criado projeto de Sistema Financeiro Pessoal

---
+++ granacerta/src/main/java/br/com/proway/granacerta/Granacerta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.proway.granacerta;
+
+/**
+ *
+ * @author Pichau
+ */
+public class Granacerta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
